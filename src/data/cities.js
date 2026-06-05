c3ec71d6c3e4999917aaf8dcdcf51c3fcb7f789f
dietervarey-code <dietervarey@gmail.com>
2026-06-05 19:14:04 +0200

    Update cities.js

--- src/data/cities.js
+++ src/data/cities.js
@@ -1,1 +1,42 @@
-
+export const cities = [
+  {
+    id: "budapest",
+    name: "Boedapest",
+    subtitle: "De Donau, bruggen en verborgen codes",
+    description:
+      "De reis start in Boedapest. Tussen statige gebouwen, bruggen en donkere zijstraten begint de zoektocht naar De Mol.",
+    mapTheme: "river-city"
+  },
+  {
+    id: "eger",
+    name: "Eger",
+    subtitle: "Wijnkelders en oude muren",
+    description:
+      "Onder Eger liggen koele wijnkelders vol gangen, vaten en verhalen. Ideaal voor wie iets wil verbergen.",
+    mapTheme: "cellar-town"
+  },
+  {
+    id: "balaton",
+    name: "Balatonmeer",
+    subtitle: "Water, wind en verwarring",
+    description:
+      "Aan het grootste meer van Hongarije wordt samenwerken plots een pak moeilijker.",
+    mapTheme: "lake"
+  },
+  {
+    id: "pecs",
+    name: "Pécs",
+    subtitle: "Mozaïeken, pleinen en patronen",
+    description:
+      "In Pécs draait alles rond kijken, onthouden en patronen herkennen.",
+    mapTheme: "art-city"
+  },
+  {
+    id: "szeged",
+    name: "Szeged",
+    subtitle: "Markt, zon en scherpe keuzes",
+    description:
+      "Szeged bruist. Maar tussen de drukte van de markt kan één verkeerde keuze veel geld kosten.",
+    mapTheme: "market"
+  }
+];
